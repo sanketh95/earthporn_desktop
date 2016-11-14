7f2ddde1b1760718a4b55066b64d51204498bad3
mike-eason <mike.eason@mail.com>
2016-11-14 09:05:24 +0000

    :bug: Fixed images that are https urls

--- Classes/FileDownloader.cs
+++ Classes/FileDownloader.cs
@@ -32,16 +32,20 @@ namespace EpApp.Classes
         
         private string SanitizeSource(string source, string extension) 
         {
+            bool isHttps = source.StartsWith("https://");
+            string http = isHttps ? "https://" : "http://";
+
             //Get rid of the http and www
             source = source.Replace("http://", "")
+                        .Replace("https://", "")
                         .Replace("www.", "");
 
             //imgur links need to start with i. and end with the extension
             //to get the actual image.
             if (source.StartsWith("imgur"))
-                source = string.Format("http://i.{0}.{1}", source, extension);
+                source = string.Format("{0}i.{1}.{2}", http, source, extension);
             else
-                source = string.Format("http://", source);
+                source = string.Format("{0}{1}", http, source);
 
             source = source.Replace("&amp;", "&");
 
